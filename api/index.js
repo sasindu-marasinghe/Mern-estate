f72828567eae91081c477575fb985a79d9b41fc9
Sasindumarasinghe1221 <sasindumarasinghe74@gmail.com>
2024-01-29 22:29:01 +0530

    Connect mongoDb database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import Express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to MongoDB!');
+}).catch((err) => {
+    console.log(err);
+})
+
+
 
 const app = Express();
 
