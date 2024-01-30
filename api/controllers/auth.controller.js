bbb8225164d525b09109ee9ec56d87ee3e502268
Sasindumarasinghe1221 <sasindumarasinghe74@gmail.com>
2024-01-30 16:33:06 +0530

    create a middleware & a function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
+import { errorHandler } from '../Utils/error.js';
 import user from '../model/user.model.js';
 import bcryptjs from 'bcryptjs';
 
-export const signup =  (req, res) => {
+export const signup =  (req, res, next) => {
  const{username, email, password} = req.body;
  const hashedPassword = bcryptjs.hashSync(password, 10);
  const newUser = new user ({username,email,password: hashedPassword});
@@ -9,7 +10,7 @@ try{
     newUser.save();
     res.status(201).json('user created successfully!');
 }  catch (error){
-    res.status(500).json(error.message);
+    next(errorHandler(550, 'error from the function '));
 }
 
 };
