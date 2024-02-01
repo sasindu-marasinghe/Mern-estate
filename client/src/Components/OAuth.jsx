fd575b8cb58e8c1fe0bf0cace9d0942c8b515d5c
Sasindumarasinghe1221 <sasindumarasinghe74@gmail.com>
2024-02-01 20:50:06 +0530

    complete profile page ui

--- client/src/Components/OAuth.jsx
+++ client/src/Components/OAuth.jsx
@@ -25,6 +25,7 @@ export default function OAuth() {
                 })
                 const data = await res.json()
                 dispatch(signInSuccess(data));
+                
         }catch (error){
             console.log('could not sign in with google',error);
         }
